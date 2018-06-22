baa543f6f43a4f5252ef78c4906f8da40e17402b
jimbonk <jimbonkgit@gmail.com>
2018-06-22 05:45:18 -0400

    First rewrite commit

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,114 @@
 extern crate rand;
 
+use rand::prelude::*;
 use std::io;
-use std::collections::HashMap;
 
-// const END_GAME: u16 = 511;
-// x_positions |= 0b000_010_000;
-// o_positions |= 0b100_000_000;
-// x_positions |= 0b000_000_001;
+struct Player {
+    name: String,
+    faction: String,
+}
+
+struct GameState<'a> {
+    board: [&'a str; 9],
+    active_player: &'a Player,
+}
 
 fn main() {
-    print_title();
-    game_loop();
+    println!("Welcome to rustactoe!");
+    let user_faction = get_user_faction();
+    let players = create_players(user_faction);
+    let state = GameState {
+        board: [" "; 9],
+        active_player: pick_active_player(&players),
+    };
+    println!("{} goes first", state.active_player.name);
+    start_game(state, &players);
 }
 
-fn print_title() {
-    println!("###################################");
-    println!("#### Tic-Tac-Toe Battleground! ####");
-    println!("###################################\n");
-}
-
-fn game_loop() {
-    let mut board = HashMap::with_capacity(2);
-    board.insert("x", 0b0);
-    board.insert("o", 0b0);
-
-    loop {
-        let _faction = choose_faction();
-        decide_first_player();
-        display_board(board);
-        break;
+fn get_user_faction() -> String {
+    let faction = prompt("Choose your faction:").to_uppercase();
+    if faction != "X" && faction != "O" {
+        return get_user_faction();
     }
-}
-
-fn prompt(message: &str) -> String {
-    let mut input = String::new();
-    println!("{}", message);
-    io::stdin().read_line(&mut input).expect("Failed to read line!");
-    input.trim().to_string()
-}
-
-fn toss_coin() -> u8 {
-    let (num, _) = rand::random::<(f64, char)>();
-    num.round() as u8
-}
-
-fn choose_faction() -> String {
-    let mut faction = prompt("Choose your faction: X, O").to_lowercase();
-    while faction != "x" && faction != "o" {
-        println!("Unknown value. Try again");
-        faction = choose_faction();
-    }
+    println!("You've chosen {}.", faction);
     faction
 }
 
-fn decide_first_player() {
-    let coin_side = prompt("Choose heads or tails:");
-    let result = match toss_coin() {
-        0 => "tails",
-        1 => "heads",
-        _ => ""
-    };
+fn prompt(message: &str) -> String {
+    let mut buffer = String::new();
+    println!("{}", message);
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer.trim_right().to_string()
+}
 
-    print!("Coin toss is {} -- ", result);
-    if result == coin_side {
-        println!("You go first!");
-    } else {
-        println!("Machine goes first!");
+fn create_players(user_faction: String) -> [Player; 2] {
+    let computer_faction = match user_faction.as_str() {
+        "X" => "O".to_string(),
+        "O" => "X".to_string(),
+        _ => String::new()
+    };
+    let user = Player { name: "user".to_string(), faction: user_faction };
+    let computer = Player { name: "computer".to_string(), faction: computer_faction };
+    [user, computer]
+}
+
+fn pick_active_player(players: &[Player]) -> &Player {
+    &players[random::<f64>().round() as usize]
+}
+
+fn start_game<'a>(mut state: GameState<'a>, players: &'a [Player]) {
+    loop {
+        match state.active_player.name.as_str() {
+            "user" => {
+                draw_board(state.board);
+                let cell = get_valid_cell(&state);
+                state.board[cell] = &state.active_player.faction;
+                check_game_state(&state);
+                state.active_player = &players[1];
+            },
+            "computer" => {
+                println!("Computer taking turn...");
+                let cell = determine_best_cell(&state);
+                state.board[cell] = &state.active_player.faction;
+                check_game_state(&state);
+                state.active_player = &players[0];
+            },
+            _ => ()
+        }
     }
 }
 
-fn display_board(board: HashMap<&str, u16>) {
-    let x_bits = format!("{:09b}", board.get("x").unwrap());
-    let o_bits = format!("{:09b}", board.get("o").unwrap());
-    let mut rows = [
-        Vec::with_capacity(3),
-        Vec::with_capacity(3),
-        Vec::with_capacity(3),
-    ];
+fn get_valid_cell(state: &GameState) -> usize {
+    let mut cell: usize = match prompt("Choose your cell (0-8):").parse() {
+        Ok(cell) => cell,
+        Err(_) => 9
+    };
 
-    for (n, (xbit, obit)) in x_bits.chars().zip(o_bits.chars()).enumerate() {
-        if xbit == '1' {
-            rows[n % 3].push("X");
-        } else if obit == '1' {
-            rows[n % 3].push("O");
-        } else {
-            rows[n % 3].push(" ");
-        }
+    if cell > 8 {
+        println!("That's not a valid board position!");
+        cell = get_valid_cell(state);
     }
 
-    let board = rows.iter()
-        .map(|row| row.join("|"))
-        .collect::<Vec<String>>()
-        .join("\n-----\n");
-    println!("\n{}", board);
+    if state.board[cell] != " " {
+        println!("{} has already been claimed!", cell);
+        cell = get_valid_cell(state);
+    }
+    cell
+}
+
+fn draw_board(board: [&str; 9]) {
+    println!("\n{}|{}|{}", board[0], board[1], board[2]);
+    println!("-+-+-");
+    println!("{}|{}|{}", board[3], board[4], board[5]);
+    println!("-+-+-");
+    println!("{}|{}|{}", board[6], board[7], board[8]);
+}
+
+fn determine_best_cell(state: &GameState) -> usize {
+    0
+}
+
+fn check_game_state(state: &GameState) {
+    let faction = &state.active_player.faction;
+    println!("Checking state for {}", faction);
 }
